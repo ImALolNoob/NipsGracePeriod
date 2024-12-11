0f0b5186b82698c4dca5f7ec5cb864ea61c89014
shane <Sm0862512@otc.edu>
2024-12-11 01:22:42 -0600

    fixed

--- src/main/java/org/novato/tabbar.java
+++ src/main/java/org/novato/tabbar.java
@@ -12,19 +12,17 @@ public class tabbar {
     }
 
     public void updateTabMenu(Player player) {
-
-        if (plugin.getConfig().getBoolean("timerEnded") == true) {
-            String header = "this is after the timer is ender";
-            String footer = "Hardcore Event is running";
-            player.setPlayerListHeaderFooter(header, footer);
-        }
-        if (((NovatoBar) plugin).isTimerEnded()) {
+        if (!((NovatoBar) plugin).isTimerEnded()) {
             String header = "§6The Event will start soon!";
             String footer = "§7The Event will start soon!";
             player.setPlayerListHeaderFooter(header, footer);
-        } if (!((NovatoBar) plugin).isTimerEnded()) {
+        }if (((NovatoBar) plugin).isTimerEnded()) {
             String header = "§6server status: §aEvent is running";
             String footer = "§7server is running";
+            player.setPlayerListHeaderFooter(header, footer);
+        } if (plugin.getConfig().getBoolean("timerEnded") == true) {
+            String header = "this is after the timer is ended";
+            String footer = "Hardcore Event is running";
             player.setPlayerListHeaderFooter(header, footer);
         }
 
